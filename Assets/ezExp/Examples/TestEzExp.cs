0ff5f8b093b7d58a1fac965400066a7a3e090678
Marc Teyssier <marc.teys@gmail.com>
2017-02-08 15:54:49 +0100

    feat(Expe): Adding a fake expe

--- Assets/ezExp/Examples/TestEzExp.cs
+++ Assets/ezExp/Examples/TestEzExp.cs
@@ -3,12 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestEzExp : MonoBehaviour {
+    public GameObject prefab;
+    GameObject lastCube = null;
 
-	void Start () {
+    public int trials = 10;
+
+    void Start () {
         EzExp.Instance.Load("The EzExp instance is working!");
-	}
+        GenerateNewCube();
+    }
 	
 	void Update () {
-		
-	}
+        CheckClick();
+
+    }
+
+    void CheckClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform.gameObject == lastCube)
+                {
+                    NextStep();
+                }
+            }
+        }
+
+    }
+
+    void NextStep()
+    {
+        Destroy(lastCube);
+        if(trials >0 )
+        {
+            GenerateNewCube();
+            trials += -1;
+        } else
+        {
+            Finished();
+        }
+    }
+
+    void Finished()
+    {
+        Debug.Log("Finished ");
+    }
+
+    void GenerateNewCube()
+    {
+        lastCube = null;
+
+        Vector3 randPos = new Vector3(Random.Range(-5, 5), Random.Range(-2, 2), Random.Range(-5, 5));
+        lastCube = GameObject.Instantiate(prefab);
+        lastCube.transform.position = randPos;
+
+    }
 }
